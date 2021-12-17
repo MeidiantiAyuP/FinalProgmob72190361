654078d9d42b1cbb1e22ceeae814ccf2eb490b51
Meidianti Ayu Permatasari <meidianti.permatasari@students.ukdw.ac.id>
2021-12-17 22:07:47 +0700

    TAMPIL TEMPLATE

---
+++ android/app/src/main/kotlin/com/example/uasprogmob72190361/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uasprogmob72190361
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant/strings.dart
@@ -0,0 +1,3 @@
+class Strings{
+    static String news_url = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=05154e77ac0042c6bfdd883c6e59ef66';
+}
